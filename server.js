8bd52a3c7f5a1c01bf5139f71b8ac66808129e52
Dmitriy Shukaliuk <mityaua@gmail.com>
2021-06-09 12:16:04 +0300

    Added changes

--- server.js
+++ server.js
@@ -1,54 +1,66 @@
 // Настройка фреймворка Express для поднятия веб-сервера
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 // Пакет для автоматического считывания переменных из .env
-require("dotenv").config();
+require('dotenv').config();
 
 // Пакет для работы с запросами
-const got = require("got");
+const got = require('got');
 
 // Импорт маршрута из отдельного файла
-const { router } = require("./booksRouter");
+// const { router } = require("./booksRouter");
 
 // Переменная для хранения порта, ключа, урла
 const PORT = process.env.PORT || 8081;
 const API_KEY = process.env.API_KEY;
-const BASE_URL = "http://api.weatherbit.io/v2.0/current";
+const BASE_URL = 'http://api.weatherbit.io/v2.0/current';
 
 // Встроенный мидлвар для автоматической обработки JSON при отправке клиентом
 app.use(express.json());
 // Встроенный мидлвар для парстинга хтмл форм
 app.use(express.urlencoded({ extended: true }));
 // Встроенный мидлвара для публичной директории
-app.use(express.static("public"));
+app.use(express.static('public'));
 // Внешний мидлвар для логирования
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 // Подключение маршрутов + наследование
 // app.use("/api", router);
 
 // Определяем маршруты (роутинг)
-app.get("/api/weather", async (req, res) => {
+app.get('/api/weather', async (req, res) => {
   try {
+    // req.query
+    // req.params
+    // req.body
+    // req.headers
     const response = await got(BASE_URL, {
       searchParams: {
-        key: "ea41bc3d393b41638cdd827b88acf3ef",
-        lat: "50.4670243",
-        lon: "30.3507212",
+        key: 'ea41bc3d393b41638cdd827b88acf3ef',
+        lat: '50.4670243',
+        lon: '30.3507212',
       },
-      responseType: "json",
+      responseType: 'json',
     });
 
-    res.json({ response: response.body });
+    const [weatherData] = response.body.data;
+
+    const {
+      city_name,
+      weather: { description },
+      temp,
+    } = weatherData;
+
+    res.json({ city_name, description, temp });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
 // Вешаем слушателя на порт
-app.listen(PORT, (error) => {
-  if (error) console.error("Error at server launch:", error);
+app.listen(PORT, error => {
+  if (error) console.error('Error at server launch:', error);
 
   console.log(`Server works at ${PORT}`);
 });
