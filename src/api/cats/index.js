017e7ecd1d9716ccaa90d0300461ce4fe474acc2
Dmitriy Shukaliuk <mityaua@gmail.com>
2021-06-10 23:40:25 +0300

    Added validation

--- src/api/cats/index.js
+++ src/api/cats/index.js
@@ -3,14 +3,25 @@ const express = require('express'); // импорт закешированног
 const router = express.Router(); // инициализация роутов
 const controllerCats = require('../../controllers/cats'); // импорт контроллеров
 
+// Импорт функций для валидации
+const {
+  validateCreateCat,
+  validateUpdateCat,
+  validateUpdateStatusCat,
+} = require('../../validation/cats');
+
 // Роутинг для кошек (получение всех, по ай ди, создание, замена, обновление, удаление)
 // Методы запросов + отдельно коллбеки внутри controllerCats
 router
   .get('/', controllerCats.getAll)
   .get('/:id', controllerCats.getById)
-  .post('/', controllerCats.create)
-  .put('/:id', controllerCats.update)
-  .patch('/:id/vaccinated', controllerCats.updateStatus)
+  .post('/', validateCreateCat, controllerCats.create)
+  .put('/:id', validateUpdateCat, controllerCats.update)
+  .patch(
+    '/:id/vaccinated',
+    validateUpdateStatusCat,
+    controllerCats.updateStatus,
+  )
   .delete('/:id', controllerCats.remove);
 
 module.exports = router;
---
+++ src/validation/cats.js
@@ -0,0 +1,51 @@
+// Валидация данных на входе (вынесено в отдельный файл)
+const Joi = require('joi');
+const { HttpCode } = require('../helpers/constans'); // Импорт констант со статусами
+
+// Валидация создания кота
+const schemaCreateCat = Joi.object({
+  name: Joi.string().alphanum().min(2).max(30).required(),
+  age: Joi.number().integer().min(1).max(40).required(),
+  isVaccinated: Joi.boolean().optional(),
+});
+
+// Валидация обновления всего кота
+const schemaUpdateCat = Joi.object({
+  name: Joi.string().alphanum().min(2).max(30).optional(),
+  age: Joi.number().integer().min(1).max(40).optional(),
+  isVaccinated: Joi.boolean().optional(),
+});
+
+// Валидация обновления статуса вакцины кота
+const schemaUpdateStatusCat = Joi.object({
+  isVaccinated: Joi.boolean().required(),
+});
+
+// Обработка ошибок валидаций
+const validate = (schema, body, next) => {
+  const { error } = schema.validate(body);
+
+  if (error) {
+    const [{ message }] = error.details;
+
+    return next({
+      status: HttpCode.BAD_REQUEST,
+      message: `Field ${message.replace(/"/g, '')}`,
+      data: 'Bad request',
+    });
+  }
+
+  next();
+};
+
+module.exports.validateCreateCat = (req, res, next) => {
+  return validate(schemaCreateCat, req.body, next);
+};
+
+module.exports.validateUpdateCat = (req, res, next) => {
+  return validate(schemaUpdateCat, req.body, next);
+};
+
+module.exports.validateUpdateStatusCat = (req, res, next) => {
+  return validate(schemaUpdateStatusCat, req.body, next);
+};
